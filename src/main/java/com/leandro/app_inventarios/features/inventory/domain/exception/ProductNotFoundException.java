c5b816bb9d32e34835065484ec301afb59d35146
Leandro Palma <psychomusicbox@gmail.com>
2026-01-13 09:07:52 -0500

    se quito palabra de mensaje de error para de producto no encontrado

--- src/main/java/com/leandro/app_inventarios/features/inventory/domain/exception/ProductNotFoundException.java
+++ src/main/java/com/leandro/app_inventarios/features/inventory/domain/exception/ProductNotFoundException.java
@@ -2,6 +2,6 @@ package com.leandro.app_inventarios.features.inventory.domain.exception;
 
 public class ProductNotFoundException extends NotFoundException {
     public ProductNotFoundException(Long id) {
-        super("Producto con id " + id + " no encontrado xd.");
+        super("Producto con id " + id + " no encontrado.");
     }
 }
